249242db1d2039cde1e366c8a0219d9a011af2fc
Miha Oleg <fire913n@gmail.com>
2018-05-07 21:04:13 +0300

    07.05.18 - smart search
    smart search

--- header.php
+++ header.php
@@ -28,10 +28,8 @@
 <div class="overlay"></div>
     <div class="wrap_mob_nav">
         <button class="close_mob_nav"></button>
-        <form method="get" class="search" action="<?php echo home_url( '/' ); ?>">
-            <input type="search" class="tp_inp" data-swplive="true" id="search" name="s" placeholder="Умный поиск" />
-            <input type="submit" class="loupe" value="" />
-        </form>
+        
+		<?php echo do_shortcode('[wcas-search-form]') ?>
 
         <?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
         <?php get_template_part( 'template-parts/navigation/navigation', 'header' ); ?>
@@ -66,10 +64,75 @@
             <div class="container">
                 <button class="open_mob_nav"></button>
                 <a href="/" class="logo"><img src="<?php bloginfo('template_url'); ?>/img/logo.png" alt="" /></a>
+				<!--
                 <form method="get" class="search" action="<?php echo home_url( '/' ); ?>">
                     <input type="search" class="tp_inp" data-swplive="true" id="search" name="s" placeholder="Умный поиск" />
                     <input type="submit" class="loupe" value="" />
                 </form>
+				-->
+				<?php echo do_shortcode('[wcas-search-form]') ?>
+				<style>
+				.dgwt-wcas-search-wrapp {
+					position: relative;
+					display: inline-block;
+					color: #444;
+					min-width: auto;
+					max-width: none;;
+					text-align: left;
+					width: 420px;
+					margin-left: 65px;
+					margin-top: 24px;
+					float: left;
+				}
+				.dgwt-wcas-ico-loupe {
+					fill: #1c1c1c;
+				}
+				.dgwt-wcas-sf-wrapp button.dgwt-wcas-search-submit {
+					background: transparent;
+				}
+				.dgwt-wcas-ico-loupe {
+					right: 0px;
+					height: 50%;
+					padding: 1px;
+					width: 54px;
+					stroke: #1c1c1c;
+					stroke-width: 3px;
+				}
+				.dgwt-wcas-sf-wrapp .dgwt-wcas-search-submit:before {
+					display: none;
+				}
+				.dgwt-wcas-sf-wrapp {
+					-webkit-border-radius: 4px;
+					border-radius: 4px;
+				}
+				.dgwt-wcas-sf-wrapp input[type="search"].dgwt-wcas-search-input {
+					-webkit-border-radius: 4px;
+					border-radius: 4px;
+				}
+				.dgwt-wcas-has-img .dgwt-wcas-sp, .dgwt-wcas-has-desc .dgwt-wcas-sp {
+					top: 0px;
+				}
+				.dgwt-wcas-preloader {
+					right: 30px!important;
+				}
+				@media (max-width: 889px){
+					.header .dgwt-wcas-search-wrapp.woocommerce {
+						display: none;
+					}
+					.wrap_mob_nav .dgwt-wcas-search-wrapp.woocommerce {
+						display: inline-block;
+					}
+					.dgwt-wcas-search-wrapp {
+						width: calc(100% - 24px);
+						margin-left: 12px;
+						margin-top: 0px;
+						margin-bottom: 10px;
+					}
+				}
+				</style>
+				
+				
+				
                 <div class="phone">
                     <a href="tel:<?php echo $phone_1; ?>"><span><?php echo substr($mytheme['phone'], 0, 7);?></span><?php echo substr($mytheme['phone'], 7);?></a>
                     <a href="tel:<?php echo $phone_2; ?>"><span><?php echo substr($mytheme['phone2'], 0, 7);?></span><?php echo substr($mytheme['phone2'], 7);?></a>
